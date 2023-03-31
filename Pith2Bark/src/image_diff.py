cb44561a5954aa3cd666c0c0558963cd4af5b70c
Muhammad Farooq <raja.muhammad.farooq@gmail.com>
2023-03-31 15:29:08 +0200

    image enhance

---
+++ Pith2Bark/src/image_diff.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+# convert the images to grayscale
+def gray_images(img1, img2)
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    return img1, img2
+
+# define the function to compute MSE between two images
+def mse(img1, img2):
+   h, w = img1.shape
+   diff = cv2.subtract(img1, img2)
+   err = np.sum(diff**2)
+   mse = err/(float(h*w))
+
+   return mse, diff
+
+def diff_visual(img1, img2):
+    img1, img2 = gray_images(img1, img2)
+    error, diff = mse(img1, img2)
+    print("Image matching Error between the two images:",error)
+
+    plt.imshow(diff)
+    plt.show()
